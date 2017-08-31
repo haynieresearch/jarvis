4807231f0081ef2d1313fd66a6d325bac8ea0367
Lance <lance@hayniemail.com>
2017-08-31 09:06:49 -0600

    hide mouse cursor

--- scripts/jarvis-xserver.sh
+++ scripts/jarvis-xserver.sh
@@ -21,7 +21,7 @@
 #with this program; if not, write to the Free Software Foundation, Inc.,
 #51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-/usr/bin/X
+/usr/bin/X -nocursor
 sleep 5
 export DISPLAY=:0
 /usr/bin/xset s off
