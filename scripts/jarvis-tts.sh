fb539d790fa9e4d34c754c3c0e34b2a761e28db6
Lance Haynie <lance@hayniemail.com>
2020-10-09 15:14:49 -0600

    updates

--- scripts/jarvis-tts.sh
+++ scripts/jarvis-tts.sh
@@ -19,4 +19,4 @@
 
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
-/usr/bin/python2.7 /opt/jarvis/scripts/tts-server.py --host $ip
+/usr/bin/python3.7 /opt/jarvis/scripts/tts-server.py --host $ip
